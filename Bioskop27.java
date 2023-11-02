93f93bce22495095d9189653842375eb32e0c3f7
ToraDigdaKristiawan <116542241+tora180405@users.noreply.github.com>
2023-11-02 14:59:48 +0700

    upload file

---
+++ Bioskop27.java
@@ -0,0 +1,25 @@
+/**
+ * Bioskop27
+ */
+public class Bioskop27 {
+
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        System.out.println(penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
+        }
+
+        System.out.printf("%s \t %s\n", penonton [0][0], penonton[0][1]); 
+        System.out.printf("%s \t %s\n", penonton [1][0], penonton [1][1]); 
+        System.out.printf("%s \t %s\n", penonton [2][0], penonton [2][1]); 
+        System.out.printf("%s \t %s\n", penonton [3] [0], penonton [3][1]);
+
+        System.out.println(penonton.length);
+        System.out.println(penonton [0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton [2].length);
+        System.out.println(penonton [3].length);
+    }
+}
