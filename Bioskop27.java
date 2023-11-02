b73e9ce5bc4e49256620de7c09f36d9cf01abecf
ToraDigdaKristiawan <116542241+tora180405@users.noreply.github.com>
2023-11-02 15:20:23 +0700

    tambah peruangan for untuk menampilkan data array baris ke 3

--- Bioskop27.java
+++ Bioskop27.java
@@ -6,6 +6,14 @@ public class Bioskop27 {
     public static void main(String[] args) {
         String[][] penonton = new String[4][2];
 
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton [1][0] = "Candra";
+        penonton [1][1] = "Dela";
+        penonton [2][0] = "Eka";
+        penonton [2][1] = "Farhan";
+        penonton [3][0] = "Gisel";
+
         System.out.println(penonton.length);
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
@@ -16,10 +24,9 @@ public class Bioskop27 {
             System.out.println("Panjang baris:" + barisPenonton.length);
         }
 
-        System.out.println(penonton.length);
-        System.out.println(penonton [0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton [2].length);
-        System.out.println(penonton [3].length);
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton [2].length; i++) { 
+            System.out.println(penonton[2][i]); 
+        }
     }
 }
