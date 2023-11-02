8ba6272ea19ed72f8c72758ae7ac6a0566640386
ToraDigdaKristiawan <116542241+tora180405@users.noreply.github.com>
2023-11-02 15:50:39 +0700

    tambah fitur menu dan validasi

--- BioskopWithScanner27.java
+++ BioskopWithScanner27.java
@@ -6,7 +6,52 @@ public class BioskopWithScanner27 {
 
         String [][] penonton = new String[4][2];
 
-        while (true) { 
+        while (true) {
+
+            System.out.println("-Menu 1: Input data penonton");
+            System.out.println("-Menu 2: Tampilkan daftar penonton");
+            System.out.println("-Menu 3: Exit ");
+            System.out.println("Pilih Menu");
+            int menu = sc.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: "); 
+                String nama = sc.nextLine(); 
+                
+                System.out.print("Masukkan baris: ");
+                int baris = sc.nextInt(); 
+                
+                System.out.print("Masukkan kolom: ");
+                int kolom = sc.nextInt();
+                sc.nextLine();
+
+                if(baris<1 || baris>4){
+                    if(kolom<1 || kolom>2){
+                        System.out.println("Kursi Tidak Tersedia");
+                        return;
+                    }
+                }
+
+                penonton [baris-1][kolom-1] = nama;
+
+                System.out.print("Input penonton lainnya? (y/n): "); 
+                String next = sc.nextLine();
+                
+                if (next.equalsIgnoreCase("n")) { 
+                    break;
+                }
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    break;
+            }
+
             System.out.print("Masukkan nama: "); 
             String nama = sc.nextLine(); 
             
